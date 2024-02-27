5c9cd81e30880afe7dde51198d82340723643fb2
zepholus <joansalograu@gmail.com>
2024-02-27 16:42:23 +0100

    change thrad and process by threads and processes

--- pySWATPlus/SWATProblem.py
+++ pySWATPlus/SWATProblem.py
@@ -36,6 +36,7 @@ class SWATProblem(Problem):
                  function_to_evaluate: Callable,
                  param_arg_name: str,
                  n_workers: int = 1,
+                 debug: bool = False,
                  **kwargs: Dict[str, Any]
                  ) -> None:
         
@@ -49,11 +50,12 @@ class SWATProblem(Problem):
           Format: function_to_evaluate(Dict[Any, Any]) -> Tuple[int, Dict[str, str]] where the first element is the error produced in the observations and the second element is a dictionary containing a user-desired identifier as the key and the location where the simulation has been saved as the value.
         - param_arg_name (str): The name of the argument within function_to_evaluate function where the current calibration parameters are expected to be passed. This parameter must be included in **kwargs
         - n_workers (int, optional): The number of parallel workers to use (default is 1).
-        - **kwargs: Additional keyword arguments, that will be passed to the objective function.
+        - debug (bool, optional): If True, print debug output during optimization (default is False).
+        - **kwargs: Additional keyword arguments, that will be passed to function_to_evaluate.
         
         Returns:
         None
         """
         
-        SWATProblemMultimodel.__init__(self, params, function_to_evaluate, param_arg_name, n_workers, None, None, None, None, None, **kwargs)
+        SWATProblemMultimodel.__init__(self, params, function_to_evaluate, param_arg_name, n_workers, None, None, None, None, None, debug **kwargs)
 
--- pySWATPlus/SWATProblemMultimodel.py
+++ pySWATPlus/SWATProblemMultimodel.py
@@ -57,12 +57,13 @@ class SWATProblemMultimodel(Problem):
                  function_to_evaluate: Callable,
                  param_arg_name: str,
                  n_workers: int = 1,
-                 parallelization: str = 'thread',
+                 parallelization: str = 'threads',
                  ub_prior: Optional[List[int]] = None,
                  lb_prior: Optional[List[int]] = None,
                  function_to_evaluate_prior: Optional[Callable] = None, #Must get X (np.ndarray) as mandatory argument
                  args_function_to_evaluate_prior: Optional[Dict[str, Any]] = None,  #X does not have to be in here, it's added in the function
                  param_arg_name_to_modificate_by_prior_function: Optional[str] = None,  #param that is modified in kwargs by the return of the prior function
+                 debug: bool = False,
 
                  **kwargs: Dict[str, Any]
                  ) -> None:
@@ -77,13 +78,14 @@ class SWATProblemMultimodel(Problem):
           Format: function_to_evaluate(Dict[Any, Any]) -> Tuple[int, Dict[str, str]] where the first element is the error produced in the observations and the second element is a dictionary containing a user-desired identifier as the key and the location where the simulation has been saved as the value.
         - param_arg_name (str): The name of the argument within function_to_evaluate function where the current calibration parameters are expected to be passed. This parameter must be included in **kwargs
         - n_workers (int, optional): The number of parallel workers to use (default is 1).
-        - parallelization (str, optional): The parallelization method to use ('thread' or 'process') (default is 'thread').
+        - parallelization (str, optional): The parallelization method to use ('threads' or 'processes') (default is 'threads').
         - ub_prior (List[int], optional): Upper bounds list of calibrated parameters of the prior model. Default is None.
         - lb_prior (List[int], optional): Lower bounds list of calibrated parameters of the prior model. Default is None.
         - function_to_evaluate_prior (Callable, optional): Prior function to be used for modifying parameters before SWAT+ simulation. Must take the name indicated by args_function_to_evaluate_prior as a mandatory argument, and must be a np.ndarray, so in the source code the following is done: function_to_evaluate_prior(args_function_to_evaluate_prior = np.ndarray, ...). Must return a value that will be used to modify a parameter in the kwargs dictionary. Default is None. 
         - args_function_to_evaluate_prior (Dict[str, Any], optional): Additional arguments for function_to_evaluate_prior. args_function_to_evaluate_prior does not have to be included here. This dictionary will be unpacked and passed as keyword arguments of  args_function_to_evaluate_prior.
         - param_arg_name_to_modificate_by_prior_function (str, optional): Parameter modified in kwargs by the return of function_to_evaluate_prior, so in the source code the following is done: kwargs[param_arg_name_to_modificate_by_prior_function] = function_to_evaluate_prior(...)
-        - **kwargs: Additional keyword arguments, that will be passed to the objective function.
+        - debug (bool, optional): If True, print debug output during optimization (default is False).
+        - **kwargs: Additional keyword arguments, that will be passed to function_to_evaluate.
 
         
         Returns:
@@ -119,6 +121,7 @@ class SWATProblemMultimodel(Problem):
         self.function_to_evaluate_prior = function_to_evaluate_prior
         self.args_function_to_evaluate_prior = args_function_to_evaluate_prior
         self.param_arg_name_to_modificate_by_prior_function = param_arg_name_to_modificate_by_prior_function
+        self.debug = debug
                 
         
         super().__init__(n_var=n_vars, n_obj=1, n_constr=0, xl=lb, xu=ub, elementwise_evaluation=False)
@@ -143,8 +146,9 @@ class SWATProblemMultimodel(Problem):
         Returns:
         None
         """
-
-        print('starting _evaluate')
+        if self.debug:
+            print('starting _evaluate')
+        
         args_array = []
 
         for x in X:
@@ -174,14 +178,14 @@ class SWATProblemMultimodel(Problem):
             args_array.append(copy.deepcopy(self.kwargs))
         
         #F = self.pool.map(self.fuction_to_evaluate, args_array)
-        if self.parallelization == 'thread':
+        if self.parallelization == 'threads':
             with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
                 F = list(executor.map(self.function_to_evaluate, args_array))      
-        elif self.parallelization == 'process':
+        elif self.parallelization == 'processes':
             with multiprocessing.Pool(self.n_workers) as pool:
                 F = list(pool.map(self.function_to_evaluate, args_array))
         else:
-            raise ValueError("parallelization must be 'thread' or 'process'")  
+            raise ValueError("parallelization must be 'threads' or 'processes'")  
 
                 
         errors, paths = zip(*F)
@@ -189,27 +193,32 @@ class SWATProblemMultimodel(Problem):
         errors_array = np.array(errors)
         paths_array = np.array(paths)
 
-        print(errors_array)
+        if self.debug:
+            print(errors_array)
 
         #replace nan error by 1e10 (infinity)
         errors_array[np.isnan(errors_array)] = 1e10
 
         #check if nans and print message
-        if np.isnan(errors_array).any():
+        if np.isnan(errors_array).any() and self.debug:
             print('ERROR: some errors are nan')
             print(errors_array)
             print(paths_array)
 
-        print('adding solutions')
+        if self.debug:
+            print('adding solutions')
 
         add_solutions(paths_array, errors_array)
     
-        print('exit adding solutions')
+        if self.debug:
+            print('exit adding solutions')
 
         #minimitzar error 
         out["F"] = errors_array
+        
+        if self.debug:
+            print('returning from _evaluate')
 
-        print('returning from _evaluate')
         
     
 
