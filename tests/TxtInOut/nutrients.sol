3ba90cd6886ed51998ed8f48b73a44d62f410f0c
Debasish Pal <48341250+debpal@users.noreply.github.com>
2025-08-08 14:04:55 +0300

    add TxtInOut folder to simplify sample_data

---
+++ tests/TxtInOut/nutrients.sol
@@ -0,0 +1,3 @@
+nutrients.sol: written by SWAT+ editor v3.0.8 on 2025-07-29 21:28 for SWAT+ rev.61.0.1
+name                    exp_co         lab_p       nitrate    fr_hum_act       hum_c_n       hum_c_p        inorgp    watersol_p         h3a_p     mehlich_p  bray_strong_p  description
+soilnut1               0.00050       5.00000       7.00000       0.02000      10.00000      80.00000       3.50000       0.15000       0.25000       1.20000       0.85000  
