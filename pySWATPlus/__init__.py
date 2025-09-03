04d0bc4c5484cf1c3176fa4efeeb43a3c318aacf
zepholus <joansalograu@gmail.com>
2025-09-03 20:57:30 +0200

    Adding Version & Build Workflow for pySWATPlus Fixes #49

--- pySWATPlus/__init__.py
+++ pySWATPlus/__init__.py
@@ -2,10 +2,12 @@ from .txtinoutreader import TxtinoutReader
 from .filereader import FileReader
 from .sensitivityanalyzer import SensitivityAnalyzer
 from .performance_metrics import PerformanceMetrics
+from ._version import version as __version__
 
 __all__ = [
     'TxtinoutReader',
     'FileReader',
     'SensitivityAnalyzer',
-    'PerformanceMetrics'
+    'PerformanceMetrics',
+    '__version__'
 ]
