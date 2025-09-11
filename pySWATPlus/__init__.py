c64bef9ca0c4fd6ab61ffe1d12c72af2c0311afd
zepholus <joansalograu@gmail.com>
2025-09-11 17:46:33 +0200

    get __version__ form importlib.metadata

--- pySWATPlus/__init__.py
+++ pySWATPlus/__init__.py
@@ -3,7 +3,14 @@ from .filereader import FileReader
 from .sensitivityanalyzer import SensitivityAnalyzer
 from .calsensitivityanalyzer import CalSensitivityAnalyzer
 from .performance_metrics import PerformanceMetrics
-from ._version import version as __version__
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # If package is not installed yet (e.g., during dev)
+    __version__ = "0.0.0"
+
 
 __all__ = [
     'TxtinoutReader',
